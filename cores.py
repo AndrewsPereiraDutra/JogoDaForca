2fa0ec6a367cb7dca85d44afee88cb3a8ebbeb76
Andrews <andrewsdutra25@gmail.com>
2019-11-16 00:24:49 -0300

    Iniciando novamente de novo

--- cores.py
+++
@@ -1,5 +0,0 @@
-class cores():
-    def __init__(self):
-        self.branco = (255,255,255)
-        self.preto = (0,0,0)
-        
--- imagens.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class imagens():                                    #Imagem de fundo do menu
-    def __init__(self):
-        self.menu = pygame.image.load("imagens/menu.jpg") # Pegar imagem do diretorio
-        self.game = pygame.image.load("imagens/Ngame_back.png")
-        self.traco = pygame.image.load("imagens/traços3.png")
